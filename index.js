947eff9aa4b5c4f4a297e226fd6f8fc0011699c6
Imtiaz Akib <imtiazakib007@gmail.com>
2022-04-21 16:13:24 +0600

    finish

--- index.js
+++ index.js
@@ -12,22 +12,23 @@ app.use(express.json());
 
 
 
-const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.vwx9p.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.tsciz.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
+
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
 
 async function run() {
     try {
         await client.connect();
-        const serviceCollection = client.db('geniusCar').collection('service');
+        const serviceCollection = client.db('carMechanic').collection('service');
 
-        app.get('/service', async (req, res) => {
+        app.get('/services', async (req, res) => {
             const query = {};
             const cursor = serviceCollection.find(query);
             const services = await cursor.toArray();
             res.send(services);
         });
 
-        app.get('/service/:id', async(req, res) =>{
+        app.get('/services/:id', async(req, res) =>{
             const id = req.params.id;
             const query={_id: ObjectId(id)};
             const service = await serviceCollection.findOne(query);
@@ -35,14 +36,14 @@ async function run() {
         });
 
         // POST
-        app.post('/service', async(req, res) =>{
+        app.post('/services', async(req, res) =>{
             const newService = req.body;
             const result = await serviceCollection.insertOne(newService);
             res.send(result);
         });
 
         // DELETE
-        app.delete('/service/:id', async(req, res) =>{
+        app.delete('/services/:id', async(req, res) =>{
             const id = req.params.id;
             const query = {_id: ObjectId(id)};
             const result = await serviceCollection.deleteOne(query);
